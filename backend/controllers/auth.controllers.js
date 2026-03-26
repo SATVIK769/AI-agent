8306ee49055e70edc73e160cd7cf0e14680de98d
SATVIK GUPTA <156520515+SATVIK769@users.noreply.github.com>
2026-03-26 18:48:50 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -24,8 +24,8 @@ export const signUp = async(req, res) => {
         res.cookie("token", token, {
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"lax",
-            secure:false
+            sameSite:"None",
+            secure:true
         })
 
         // return res.status(200).json(user)
@@ -61,8 +61,8 @@ export const Login = async(req, res) => {
         res.cookie("token", token, {
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"lax",
-            secure:false
+            sameSite:"None",
+            secure:true
         })
 
 
