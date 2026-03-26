dcd1f047642af3a5480a01b44eb47d726da10dc4
SATVIK GUPTA <156520515+SATVIK769@users.noreply.github.com>
2026-03-26 18:47:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import userRouter from "./routes/user.routes.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-agent-69nx.onrender.com",
     credentials: true
 }))
 
