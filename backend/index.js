e1da52e90c9ef6129ee98eabf8e8567685cbcb35
SATVIK GUPTA <156520515+SATVIK769@users.noreply.github.com>
2026-04-16 23:56:20 +0530

    Update CORS origin for the application

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import userRouter from "./routes/user.routes.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://ai-agent-69nx.onrender.com",
+    origin:"https://ai-notes-generatorclient.onrender.com",
     credentials: true
 }))
 
